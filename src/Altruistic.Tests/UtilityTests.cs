2a7e4658d587e7454042bbaefd8069f929c911b6
ashrafMageed <ashraf_mageed@hotmail.com>
2013-11-23 00:29:24 +0000

    Refactoring: Moved some methods to utility class

---
+++ src/Altruistic.Tests/UtilityTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentAssertions;
+
+namespace Altruistic.Tests
+{
+    public class UtilityTests
+    {
+        public class GetMethodTests
+        {
+            public void GivenAMethodCallExpression_ShouldExtractMethodFromTheExpression()
+            {
+                // Arrange
+                Expression<Func<string, string>> expression = s => s.Insert(3, "abc");
+
+                // Act
+                var method = Utility.GetMethod(expression);
+
+                // Assert
+                method.Name.Should().Be("Insert");
+            }
+
+            public void GivenAFunc_ShouldExtractMethodFromTheFunc()
+            {
+                // Arrange
+                Func<string> func = DummyMethod;
+
+                // Act
+                var method = Utility.GetMethod(func);
+
+                // Assert
+                method.Name.Should().Be("DummyMethod");
+            }
+
+            public string DummyMethod()
+            {
+                return String.Empty;
+            }
+
+
+            public void GivenANonMethodCallExpression_ShouldThrowAnArgumentException()
+            {
+                // Arrange
+                Expression<Func<string, int>> expression = s => s.Length;
+
+                // Act
+                Action act = () => Utility.GetMethod(expression);
+
+                // Assert
+                act.ShouldThrow<ArgumentException>();
+            }
+        }
+    }
+}
---
+++ src/Altruistic/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Altruistic
+{
+    public static class Utility
+    {
+        public static MethodInfo GetMethod(LambdaExpression expression)
+        {
+            var methodCallExpression = expression.Body as MethodCallExpression;
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentException("expression must be a method call");
+            }
+            return methodCallExpression.Method;
+        }
+
+        public static string GetMethodName(LambdaExpression expression)
+        {
+            return GetMethod(expression).Name;
+        }
+
+        public static MethodInfo GetMethod(Func<object> expression)
+        {
+            return expression.Method;
+        }
+    }
+}
